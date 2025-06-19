1d70c7cd0c438d0be64ec97fd0acfa371e682c25
snk978 <kksnk383@gmail.com>
2025-06-19 15:36:13 +0530

    Fix: Added root route to resolve 404 on /

--- studio_bd/urls.py
+++ studio_bd/urls.py
@@ -1,24 +1,14 @@
-"""
-URL configuration for studio_bd project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.http import HttpResponse
+
+# Simple home view to serve the root URL
+def home(request):
+    return HttpResponse("🚀 Studio Fliplora is live!")
 
 urlpatterns = [
+    path('', home, name='home'),  # Root URL now responds with 200 OK
     path('admin/', admin.site.urls),
-    path("ex",include("studio.urls")),
-    path('api/',include("api_studio.urls")),
+    path('ex/', include('studio.urls')),  # Adjust if you meant something else
+    path('api/', include('api_studio.urls')),
 ]
