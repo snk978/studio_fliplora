d0267bf55b0efe196cf7de3d6d8765c1b47a7807
snk978 <kksnk383@gmail.com>
2025-06-20 14:11:41 +0530

    updated ui

--- studio/views.py
+++ studio/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def ex(request):
-   return render(request,"studio/ex.html")
+   return render(request,"studio/ex2.html")
